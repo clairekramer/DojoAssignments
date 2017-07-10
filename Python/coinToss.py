d4371c13917b2ba4bcb9238bb106d3291dafbf29
Claire <kramerclaire@me.com>
2017-07-09 20:02:06 -0700

    Updated Coin Toss assignment

--- Python/coinToss.py
+++ Python/coinToss.py
@@ -1,16 +1,17 @@
-def coinToss():
-    print 'Starting the program...'
+#Coin Tosses
+def toss():
+    import random
+    print "Starting the program..."
     heads = 0
     tails = 0
-    for x in range(1,5001):
-        import random
-        num = round(random.random())
-        if num == 1:
+    for each in range(1, 5001):
+        x = random.randint(0,1)
+        if x == 0:
             heads = heads + 1
-            print "Atempt #{}: Throwing a coin... It's a head! ... Got {} head(s) so far and {} tail(s) so far".format(x,heads,tails)
+            print "Attempt #{}: Throwing a coin... It's heads! ... Got {} head(s) so far and {} tail(s) so far".format(each,heads,tails)
         else:
             tails = tails + 1
-            print "Atempt #{}: Throwing a coin... It's a tail! ... Got {} head(s) so far and {} tail(s) so far".format(x,heads,tails)
-    print 'Ending the program, thank you!'
+            print "Attempt #{}: Throwing a coin... It's tails! ... Got {} head(s) so far and {} tail(s) so far".format(each,heads,tails)
+    print "Ending the program. Thank you!"
 
-coinToss()
+toss()
