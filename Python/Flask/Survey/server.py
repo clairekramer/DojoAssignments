3a570d82b3f71636a73bf9fa146f802fdf590a38
Claire <kramerclaire@me.com>
2017-07-22 21:08:14 -0700

    Validated Survey

--- Python/Flask/Survey/server.py
+++ Python/Flask/Survey/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
 app.secret_key = "supersecret"
 
@@ -10,6 +10,14 @@ def index():
 def survey():
     print request.form['name']
     session['survey_results'] = request.form
+    if len(request.form['name']) < 1:
+        flash('Name cannot be empty!')
+        return redirect('/')
+    else:
+        flash('Success! Your name is {}'.format(request.form['name']))
+    if len(request.form['comment']) > 120:
+        flash('Comment cannot be longer than 120 characters!')
+        return redirect('/')
     return redirect('/result')
 
 @app.route('/result')
